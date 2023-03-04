64f1a4a9ba04f1041786534c471ae4a0e882bab4
ImreW <94045136+imrewang@users.noreply.github.com>
2023-03-04 13:47:35 +0800

    Add files via upload

---
+++ Code/playingmenu.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "gamelabel.h"
+#include "config.h"
+#include <QWidget>
+#include <QPushButton>
+
+class PlayingMenu : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit PlayingMenu(QWidget* parent = nullptr);
+protected:
+    ClickLabel* m_button[3]; // 游戏菜单的三个选项
+    QWidget* m_shadowDlg; // 阴影遮罩
+signals:
+    void restartGame()const; // 重新开始游戏
+    void exitGame()const; // 退出游戏
+};
+
+class ResultMenu : public QWidget
+{
+    Q_OBJECT
+public:
+    ClickLabel* m_button[2]; // 结束菜单的三个选项
+    HintLabel* m_resultLabel; // 游戏结果的hint
+    QWidget* m_shadowDlg; // 阴影遮罩
+public:
+    explicit ResultMenu(QWidget* parent = nullptr);//蓝方获胜1 红方获胜0
+    void setResult(const bool blueWinOrNot, const bool AIOpenOrNot);
+signals:
+    void restartGame(); // 重新开始游戏
+    void exitGame(); // 退出游戏
+};
